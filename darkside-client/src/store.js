c9b34d5298a30ad195388b6d77d611013442df41
James <james.randolph2016@gmail.com>
2019-02-12 12:54:40 -0500

    add react-redux, redux, thunk

--- darkside-client/src/store.js
+++ darkside-client/src/store.js
@@ -5,4 +5,17 @@ import {
   } from 'redux';
   import thunk from 'redux-thunk';
 
-  
+import snowboards from './reducers/snowboards';
+import snowboardFormData from './reducers/snowboardFormData';
+
+const reducers = combineReducers({
+  snowboards,
+  snowboardFormData
+});
+const middleware = [thunk];
+
+export default createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(...middleware),
+);
