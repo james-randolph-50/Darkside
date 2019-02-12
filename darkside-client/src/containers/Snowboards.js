658d07e30783f40261309c1bc32571c1e43684b0
James <james.randolph2016@gmail.com>
2019-02-12 11:30:49 -0500

    snowboards containeR

---
+++ darkside-client/src/containers/Snowboards.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import SnowboardCard from '../components/SnowboardCard';
+import SnowboardForm from './SnowboardForm';
+import { getSnowboards } from '../actions/snowboards';
+import './Snowboards.css';
+
+class Snowboards extends Component {
+
+    componentDidMount() {
+      this.props.getSnowboards()
+    }
+
+    render() {
+        return (
+          <div className="SnowboardsContainer">
+            <h1>Snowboards</h1>
+            {this.props.snowboards.map(snowboard => <SnowboardCard key={snowboard.id} snowboard={snowboard} />)}
+            <SnowboardForm />
+          </div>
+        );
+      }
+    }
+
+    const mapStateToProps = (state) => {
+        return ({
+          snowboards: state.snowboards
+        })
+      }
+      
+       export default connect(mapStateToProps, { getSnowboards })(Snowboards);
