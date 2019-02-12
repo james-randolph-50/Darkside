82a034623d8f9259a8e24d4e2900aed1d616a586
James <james.randolph2016@gmail.com>
2019-02-12 12:22:13 -0500

    add env and janws

--- darkside-client/src/containers/App.js
+++ darkside-client/src/containers/App.js
@@ -3,7 +3,24 @@ import React, { Component } from 'react';
 import Snowboards from './Snowboards';
 import './App.css';
 
+const API_URL = process.env.REACT_APP_API_URL;
+
 class App extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            snowboards: []
+        }
+    }
+
+    componentDidMount() {
+        fetch(`${API_URL}/snowboards`)
+          .then(response => response.json())
+          .then(snowboards => this.setState({ snowboards }))
+    }
+
     render() {
       return (
         <div className="App">
