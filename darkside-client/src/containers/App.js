608156da5d0a0b4c209aba2caa90128927058b98
James <james.randolph2016@gmail.com>
2019-02-12 11:10:04 -0500

    app.js moved and created, controller actions mapped out

---
+++ darkside-client/src/containers/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Snowboards from './Snowboards';
+import './App.css';
+
+class App extends Component {
+    render() {
+      return (
+        <div className="App">
+          <Snowboards />
+        </div>
+      );
+    }
+  }
+  
+  export default App;
